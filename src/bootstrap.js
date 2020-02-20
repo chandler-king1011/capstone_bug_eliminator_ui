f6fac818a6dbea4153bb5f325880636e849eef5c
Chandler King <ckmotox356@gmail.com>
2020-02-19 17:46:58 -0700

    Removed the redux developer tools middleware to fix app errors on some browsers.

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -29,7 +29,7 @@ import BugDetail from "./components/bugs/bugDetail";
 
 
 const middleware = [thunk];
-const store = createStore(reducers, compose(applyMiddleware(...middleware), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()));
+const store = createStore(reducers, compose(applyMiddleware(...middleware)));
 
 const persistor = persistStore(store);
 
